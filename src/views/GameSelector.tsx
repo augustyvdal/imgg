d9c6c3a60dbc4be5648fbca4559153b0de9fb2a1
Gustav Blomdahl <blomdahlgustav@gmail.com>
2025-10-13 15:05:32 +0200

    home_page_test

--- src/views/GameSelector.tsx
+++ src/views/GameSelector.tsx
@@ -1,6 +1,14 @@
 function GameSelector() {
     return (
-        <h1>Home</h1>
+        <div class="relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] bg-white">
+            <div class="col-start-3 row-start-3 flex max-w-lg flex-col place-content-center">
+                <img src="src\images\logo.png"/>
+                <div class="col-start-3 row-start-3 flex max-w-lg flex-col">
+                    <div class='text-stone-950 flex justify-center'>The internet movie guessing game</div>
+                </div>
+            </div>
+        </div>
+        
     );
 }
 
