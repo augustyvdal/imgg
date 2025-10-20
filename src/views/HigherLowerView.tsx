7caeda81a2bbf3241a8ff4617f354854a020ce2b
Gustav Blomdahl <blomdahlgustav@gmail.com>
2025-10-21 01:03:28 +0200

    new higher lower

--- src/views/HigherLowerView.tsx
+++ src/views/HigherLowerView.tsx
@@ -53,38 +53,43 @@ export default function HigherLowerView({contentA, contentB, score, category, me
                     {loading ? (
                         <Spinner />
                     ) : (
-                        <div className="bg-white dark:bg-gray-800 p-8 rounded-lg w-full max-w-7xl grid grid-cols-[1fr_auto_1fr] items-start gap-x-6">
-                            <div className="flex flex-col items-center gap-1">
-                                <h3 className="text-black dark:text-white text-xl font-sans font-bold text-center w-full">{contentA?.title || contentA?.name}</h3>
-                                <img className="h-[350px] w-auto object-contain mb-2"
-                                    src={`https://image.tmdb.org/t/p/w200${contentA?.poster_path}`}
-                                    alt={contentA?.title || contentA?.name}
-                                />
-                                <p className="text-black dark:text-white text-2xl font-sans font-bold">Rating: {contentA?.vote_average}</p>
-                            </div>
+                        <div className="flex flex-col items-center gap-4">
+                            <h2 className="mt-4 w-full max-w-xs bg-col2 text-white font-bold text-center text-lg px-4 py-2 rounded-lg shadow">Current Score: {score}</h2>
 
-                                <div className="flex flex-col items-center justify-between h-3/7">
-                                    <h2 className="mt-4 w-full max-w-xs bg-col2 text-white font-bold text-center text-lg px-4 py-2 rounded-lg shadow">Current Score: {score}</h2>
-                                    <div className="flex flex-col items-center gap-4">
-                                        {message && <p className="text-black dark:text-white text-2xl font-sans font-bold">{message}</p>}
-                                        {gameOver && <button className="btn--default" onClick={prepareNewGame}>Play Again!</button>}
+                            <div className="bg-white dark:bg-gray-800 p-8 rounded-lg w-full max-w-7xl  items-start gap-x-6">
+                                <div className="flex flex-row justify-center gap-x-5">
+                                    <div className="flex flex-col gap-1 width-50 height-350">
+                                        <h3 className="text-black dark:text-white text-xl font-sans font-bold text-center relative inset-x-0 top-0 h-16">{contentA?.title || contentA?.name}</h3>
+                                        <img className="h-[350px] w-auto object-contain mb-2 justify-center"
+                                            src={`https://image.tmdb.org/t/p/w200${contentA?.poster_path}`}
+                                            alt={contentA?.title || contentA?.name}
+                                        />
+                                        <p className="text-black dark:text-white text-xl font-sans font-bold text-center relative inset-x-0 bottom-0 h-16">Rating: {contentA?.vote_average}</p>
                                     </div>
-                                </div>
+                                    
+                                    
+                                    <p className="text-black dark:text-white text-3xl font-sans font-bold text-center self-center">VS</p>
+                                    
+                                    
 
-
-                                <div className="flex flex-col items-center gap-1">
-                                    <h3 className="text-black dark:text-white text-xl font-sans font-bold text-center w-full">{contentB?.title || contentB?.name}</h3>
-                                    <img className="h-[350px] w-auto object-contain mb-2"
-                                        src={`https://image.tmdb.org/t/p/w200${contentB?.poster_path}`}
-                                        alt={contentB?.title || contentB?.name}
-                                    />
-                                    <p className="text-black dark:text-white text-2xl font-sans font-bold">{showRatings ? `Rating: ${contentB?.vote_average}` : "Rating: ???"}</p>
-                                    <div className="flex gap-3 mt-4">
+                                    <div className="flex flex-col gap-1 width-50 height-350">
+                                        <h3 className="text-black dark:text-white text-xl font-sans font-bold text-center relative inset-x-0 top-0 h-16">{contentB?.title || contentB?.name}</h3>
+                                        <img className="h-[350px] w-auto object-contain mb-2 min-w-50"
+                                                src={`https://image.tmdb.org/t/p/w200${contentB?.poster_path}`}
+                                                alt={contentB?.title || contentB?.name}
+                                            />
+                                        <p className="text-black dark:text-white text-xl font-sans font-bold text-center relative inset-x-0 bottom-0 h-16">{showRatings ? `Rating: ${contentB?.vote_average}` : "Rating: ???"}</p>
                                         <button className="btn--default" onClick={() => onGuess("higher")} disabled={buttonsDisabled}>Higher</button>
                                         <button className="btn--default" onClick={() => onGuess("lower")} disabled={buttonsDisabled}>Lower</button>
                                     </div>
                                 </div>
                             </div>
+
+                            <div className="flex flex-col items-center gap-4">
+                                        {message && <p className="text-black dark:text-white text-2xl font-sans font-bold">{message}</p>}
+                                        {gameOver && <button className="btn--default" onClick={prepareNewGame}>Play Again!</button>}
+                            </div>
+                        </div>
 
                     )}
                 </>
