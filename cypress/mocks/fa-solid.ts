920bfeed74091bebdc01f4a567e500ec294c4786
yvdal <yvdal@kth.se>
2025-10-23 00:00:23 +0200

    Added root in backend server.ts

---
+++ cypress/mocks/fa-solid.ts
@@ -0,0 +1,15 @@
+const ICON = {} as any;
+
+export const faInfoCircle = ICON;
+export const faUser = ICON;
+export const faSignOut = ICON;
+export const faTrophy = ICON;
+export const faUpload = ICON;
+export const faTrash = ICON;
+export const faPlus = ICON;
+export const faTimes = ICON;
+export const faXmark = ICON;
+export const faBars = ICON;
+export const faSpinner = ICON;
+export const faChevronRight = ICON;
+export const faChevronLeft = ICON;
